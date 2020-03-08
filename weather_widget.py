4e2bb6fb35e657225317106e811c9c1e8c3f58cc
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2020-03-08 01:00:26 -0500

    updates and refactor

--- weather_widget.py
+++ weather_widget.py
@@ -1,173 +1,180 @@
-from datetime import datetime
-import gzip
-import pickle
-import json
-import os
-from PIL import Image
-import requests
+import datetime
+import base64
 import PySimpleGUI as sg
+import requests
 
 sg.ChangeLookAndFeel('light blue')
 GOLD = "#FFC13F"
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        with open(relative_path) as f:
-            return relative_path
-    except Exception:
-        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-        return os.path.join(base_path, relative_path)
+APP_DATA = {
+    'City': 'Charlotte',
+    'Country': 'US',
+    'Postal': 28273,
+    'Description': 'clear skys',
+    'Temp': 101.0,
+    'Feels Like': 72.0,
+    'Wind': 0.0,
+    'Humidity': 0,
+    'Precip 1hr': 0.0,
+    'Pressure': 0,
+    'Updated': 'Not yet updated',
+    'Icon': None,
+    'Units': 'Imperial'
+}
+
+API_KEY = "786af9fe6332d5da4af55ec5a90c4d4e"
+
+def create_endpoint(endpoint_type=0):
+    """ Create the api request endpoint
+    {0: default, 1: zipcode, 2: city_name}"""
+    if endpoint_type == 1:
+        try:
+            endpoint = f"http://api.openweathermap.org/data/2.5/weather?zip={APP_DATA['Postal']},us&appid={API_KEY}&units={APP_DATA['Units']}"
+            return endpoint
+        except ConnectionError:
+            return
+    elif endpoint_type == 2:
+        try:
+            endpoint = f"http://api.openweathermap.org/data/2.5/weather?q={APP_DATA['City']},us&APPID={API_KEY}&units={APP_DATA['Units']}"
+            return endpoint
+        except ConnectionError:
+            return
+    else:
+        return
 
 
-# Get saved list of us cities
-with open(resource_path('app data/cities.pkl'), 'rb') as file:
-    CITIES = pickle.load(file)
-
-# Get api key
-with open(resource_path('app data/api-key.txt')) as f:
-    API_KEY = f.read()
-
-# Default parameters and endpoint
-with open(resource_path('app data/default_city_key.pkl'), 'rb') as file:
-    CITY_KEY = pickle.load(file)
-
-CITY_DATA = CITIES[CITY_KEY]
-CITY = CITY_DATA[2]
-CITY_STR = f"{CITY_DATA[2]},{CITY_DATA[0]}"
-UNITS = "imperial"  # default: kelvin, metric: celsius, imperial: fahrenheit
-
-
-def update_list_of_cities():
-    """ Update the list of cities from the openweathermap api list """
-    url = "http://bulk.openweathermap.org/sample/city.list.json.gz"
-    with gzip.open(requests.get(url, stream=True).raw, 'rb') as file:
-        data = json.load(file)
-
-    city_list = [[row.get('country'), row.get('state'), row.get('name')] for row in data]   
-    city_dict = {"--".join(row): row for row in city_list if row[0] != ''}
-
-    with open(resource_path('app data/cities.pkl'), 'wb') as file:
-        pickle.dump(city_dict, file)
+def request_weather_data(endpoint):
+    """ Send request for updated weather data """
+    global APP_DATA
+    if endpoint is None:
+        return
+    else:
+        response = requests.get(endpoint)
+    
+    if response.ok:
+        weather = response.json()
+        APP_DATA['City'] = weather['name'].title()
+        APP_DATA['Description'] = weather['weather'][0]['description']
+        APP_DATA['Temp'] = "{:,.0f}°F".format(weather['main']['temp'])
+        APP_DATA['Humidity'] = "{:,d}%".format(weather['main']['humidity'])
+        APP_DATA['Pressure'] = "{:,d} hPa".format(weather['main']['pressure'])
+        APP_DATA['Feels Like'] = "{:,.0f}°F".format(weather['main']['feels_like'])
+        APP_DATA['Wind'] = "{:,.1f} m/h".format(weather['wind']['speed'])
+        APP_DATA['Precip 1hr'] = None if not weather.get('rain') else "{:,d} mm".format(weather['rain']['1hr'])
+        APP_DATA['Updated'] = 'Updated: ' + datetime.datetime.now().strftime("%B %d %I:%M:%S %p")
+        
+        icon_url = "http://openweathermap.org/img/wn/{}@2x.png".format(weather['weather'][0]['icon'])
+        print(icon_url)
+        APP_DATA['Icon'] = base64.b64encode(requests.get(icon_url).content)        
 
 
-def request_weather_data(city_str, units, api_key):
-    """ Request weather data from endpoint"""
-    endpoint = f"http://api.openweathermap.org/data/2.5/weather?q={city_str}&APPID={api_key}&units={units}"
-    request = requests.get(endpoint)
-    if request.ok:
-        weather = request.json()
-
-    app_data = {}
-
-    # Get all relevant variables from request variable
-    app_data['City'] = weather.get('name').title()
-    app_data['Description'] = weather.get('weather')[0].get('description')
-    app_data['Temp'] = "{:,.0f}°F".format(weather.get('main').get('temp'))
-    app_data['Feels Like'] = "{:,.0f}°F".format(weather.get('main').get('feels_like'))
-    app_data['Wind'] = "{:,.1f} m/h".format(weather.get('wind').get('speed'))
-    app_data['Humidity'] = "{:,d}%".format(weather.get('main').get('humidity'))
-    app_data['Precip 1hr'] = None if not weather.get('rain') else "{} mm".format(weather.get('rain').get('1h'))
-    app_data['Pressure'] = "{:,d} hPa".format(weather.get('main').get('pressure'))
-
-    # Get the weather icon and save locally
-    weather_icon = "http://openweathermap.org/img/wn/{}@2x.png".format(weather['weather'][0]['icon'])
-    req = requests.get(weather_icon, stream=True)
-    img = Image.open(req.raw)
-    img.save(resource_path('app data/current_weather.png'))
-    app_data['Icon'] = resource_path('app data/current_weather.png')
-
-    # Get update time and date
-    app_data['Updated'] = datetime.now().strftime("%B %d %I:%M %p")
-
-    return app_data
-
-
-def layout_metric(metric):
+def metric_row(metric):
     """ Return a pair of labels for each metric """
-    label = sg.Text(metric, font=('Arial', 10), pad=(0, 0), size=(9, 1))
-    measure = sg.Text(metric, font=('Arial', 10, 'bold'), pad=(0, 0), size=(9, 1), key=metric)
-    return [label, measure]
+    lbl = sg.Text(metric, font=('Arial', 10), pad=(15, 0), size=(9, 1))
+    num = sg.Text(APP_DATA[metric], font=('Arial', 10, 'bold'), pad=(0, 0), size=(9, 1), key=metric)
+    return [lbl, num]
 
 
-def app_window(app_data):
-    """ Build application window """
-    app_data = request_weather_data(CITY_STR, UNITS, API_KEY)
+def create_window():
+    """ Create the application window """
+    col1 = sg.Column(
+        [[sg.Text(APP_DATA['City'], font=('Arial Rounded MT Bold', 18), pad=((10, 0), (50, 0)), size=(18, 1), background_color=GOLD, key='City')],
+        [sg.Text(APP_DATA['Description'], font=('Arial', 12), pad=(10, 0), background_color=GOLD, key='Description')]],
+            background_color=GOLD, key='COL1')
 
-    col1 = sg.Column([[sg.Text(app_data['City'], font=('Arial Rounded MT Bold', 14), pad=((15, 0), (25, 0)), background_color=GOLD, enable_events=True, size=(200, 1), key='City')],
-                     [sg.Text(app_data['Description'], font=('Arial', 12), pad=((15, 0), (0, 0)), background_color=GOLD, key='Description')]],
-        pad=(0, 0), size=(200, 100), background_color=GOLD, key='COL1')
-    
-    col2 = sg.Column([[sg.Image(filename=app_data['Icon'], pad=(0, 0), background_color=GOLD, size=(150, 100), key='Icon')]],
-        pad=(0, 0), size=(150, 100), background_color=GOLD, element_justification='center', key='COL2')
-    
-    col3 = sg.Column([[sg.Text(text=app_data['Temp'], font=('Haettenschweiler', 70), pad=(15, 0), key='Temp')]],
-        pad=((15, 15), (0, 0)), size=(150, 125))
-    
-    col4 = sg.Column([layout_metric('Feels Like'), layout_metric('Wind'), layout_metric('Humidity'),
-                      layout_metric('Precip 1hr'), layout_metric('Pressure')],
-        pad=(0, 0), size=(200, 125))
-    
-    col5 = sg.Column([[sg.Text(app_data['Updated'], background_color=GOLD, key='Updated')]],
-        pad=(0, 0), size=(100, 30), background_color=GOLD, key='TIME')
+    col2 = sg.Column(
+        [[sg.Text('×', font=('Arial Black', 16), pad=(0, 0), justification='right', background_color=GOLD, enable_events=True, key='-QUIT-')],
+        [sg.Image(data=APP_DATA['Icon'], pad=((5, 10), (0, 0)), size=(100, 100), background_color=GOLD, key='Icon')]],
+            element_justification='center', background_color=GOLD, key='COL2')
 
-    layout = [[col1, col2], [col3, col4], [col5]]
+    col3 = sg.Column(
+        [[sg.Text(APP_DATA['Updated'], font=('Arial', 8), background_color=GOLD, key='Updated')]],
+            pad=(10, 5), element_justification='left', background_color=GOLD, key='COL3')
 
-    window = sg.Window(layout=layout, title='WeatherApp', size=(350, 255), margins=(0, 0), element_justification='center',
-        finalize=True, return_keyboard_events=True, no_titlebar=True, grab_anywhere=True, keep_on_top=True)
+    col4 = sg.Column(
+        [[sg.Text('click to change city', font=('Arial', 8, 'italic'), background_color=GOLD, text_color='dark blue', enable_events=True, key='-CHANGE-')]],
+            pad=(10, 5), element_justification='right', background_color=GOLD, key='COL4')
 
-    for key in ['COL1', 'COL2', 'TIME']:
-        window[key].expand(expand_x=True)
+    top_col = sg.Column([[col1, col2]], pad=(0, 0), background_color=GOLD, key='TopCOL')
+
+    bot_col = sg.Column([[col3, col4]], pad=(0, 0), background_color=GOLD, key='BotCOL')
+
+    lf_col = sg.Column(
+        [[sg.Text(APP_DATA['Temp'], font=('Haettenschweiler', 90), pad=((10, 0), (0, 0)), justification='center', key='Temp')]],
+            pad=(10, 0), element_justification='center', key='LfCOL')
+
+    rt_col = sg.Column(
+        [metric_row('Feels Like'), metric_row('Wind'), metric_row('Humidity'), metric_row('Precip 1hr'), metric_row('Pressure')],
+            pad=((15, 0), (25, 5)), key='RtCOL')
+
+    layout = [[top_col], [lf_col, rt_col], [bot_col]]
+
+    window = sg.Window(layout=layout, title='Weather Widget', size=(400, 315), margins=(0, 0), finalize=True, 
+        element_justification='center', keep_on_top=True, no_titlebar=True, grab_anywhere=True)
+
+    for col in ['COL1', 'COL2', 'TopCOL', 'BotCOL', '-QUIT-']:
+        window[col].expand(expand_y=True, expand_x=True)
+
+    for col in ['COL3', 'COL4', 'LfCOL', 'RtCOL']:
+        window[col].expand(expand_x=True)
 
     return window
 
 
-def update_weather(city_str, units, api_key, window):
-    """ Update weather information on existing window """
-    app_data = request_weather_data(city_str, units, api_key)
+def change_city(window):
+    """ Change postal zip code or city for weather api """
+    global APP_DATA
+    xpos, ypos = window.current_location()
+    new_city = sg.popup_get_text(message="Enter 5-digit ZIP Code OR City Name", default_text=str(APP_DATA['Postal']), no_titlebar=True, keep_on_top=True, location=(xpos+405, ypos))
+    if new_city.isnumeric() and len(new_city) == 5:
+        APP_DATA['Postal'] = int(new_city)
+        request_weather_data(create_endpoint(1))
+        update_metrics(window)
+    else:
+        APP_DATA['City'] = new_city
+        request_weather_data(create_endpoint(2))
+        update_metrics(window)
+
+
+def update_metrics(window):
+    """ Adjust the GUI to reflect the current weather metrics """
     metrics = ['City', 'Temp', 'Feels Like', 'Wind', 'Humidity', 'Precip 1hr',
                'Description', 'Icon', 'Pressure', 'Updated']
     for metric in metrics:
-        window[metric].update(app_data.get(metric))
-
-
-def city_select_popup():
-    """ PopUP to request city from user or return default """
-    global CITY_KEY, CITY_DATA, CITY, CITY_STR
-    # us cities only
-    cities = sorted([x for x in CITIES.keys() if x.startswith("US")])
-    layout = [[sg.Combo(values=cities, default_value=CITY_KEY, size=(25, 15), font=('Arial', 14), key='SELECTION', enable_events=True)]]
-
-    window = sg.Window(layout=layout, title='Select City', keep_on_top=True, no_titlebar=True, background_color=GOLD)
-
-    _, combo_value = window.read()
-    CITY_KEY = combo_value['SELECTION']
-    CITY_DATA = CITIES[CITY_KEY]
-    CITY = CITY_DATA[2]
-    CITY_STR = f"{CITY_DATA[2]},{CITY_DATA[0]}"
-    
-    with open(resource_path('app data/default_city_key.pkl'), 'wb') as file:
-        pickle.dump(CITY_KEY, file)
-
-    window.close()
+        if metric == 'Icon':
+            window[metric].update(data=APP_DATA[metric])
+        else:
+            window[metric].update(APP_DATA[metric])
 
 
 def main(refresh_rate):
-    """ The main programm routine """
-    app_data = request_weather_data(CITY_STR, UNITS, API_KEY)
-    window = app_window(app_data)
-    timeout_minutes = refresh_rate * (60 * 1000) # refresh rate is in minutes
+    """ The main program routine """
+    timeout_minutes = refresh_rate * (60 * 1000)
 
+    # Try to get the current users ip location
+    try:
+        postal = requests.get('https://ipapi.co/json').json()['postal']
+        APP_DATA['Postal'] = postal
+        request_weather_data(create_endpoint(1))
+    except ConnectionError:
+        pass
+
+    # Create main window
+    window = create_window()
+
+    # Event loop
     while True:
-        event, _ = window.read(timeout=timeout_minutes)
-        if event in(None, 'Escape:27'):
+        event, values = window.read(timeout=timeout_minutes)
+        if event in (None, '-QUIT-'):
             break
-        if event == 'City':
-            city_select_popup()
-
-        update_weather(CITY_STR, UNITS, API_KEY, window)
+        if event == '-CHANGE-':
+            change_city(window)
+        else:
+            print(event, values)
+    
+    window.close()
 
 
 if __name__ == '__main__':
     print("Starting program...")
-    main(refresh_rate=10)
+    main(refresh_rate=5)
